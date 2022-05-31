c418b3513493af124828b5a7c7e9ce1579f903f0
bindu-sharma <57456033+bindu-sharma@users.noreply.github.com>
2022-05-30 23:56:16 -0400

    Class for creating class objects at App level

--- app/src/main/java/com/silverorange/videoplayer/MyApp.java
+++ app/src/main/java/com/silverorange/videoplayer/MyApp.java
@@ -2,7 +2,7 @@ package com.silverorange.videoplayer;
 
 import android.app.Application;
 
-//Class for creating class objects ar App level
+//Class for creating class objects at App level
 
 public class MyApp extends Application {
     private NetworkingService networkingService = new NetworkingService();
